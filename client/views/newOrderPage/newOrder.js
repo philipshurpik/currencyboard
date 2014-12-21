6fc182168268fb116a825accd87a9aa382558a00
philipshurpik <philip.shurpik@gmail.com>
2014-12-21 09:53:33 +0200

    Rate input improvements

--- client/views/newOrderPage/newOrder.js
+++ client/views/newOrderPage/newOrder.js
@@ -1,5 +1,21 @@
 Template.newOrderPage.rendered = function() {
     Session.set('headerState', { text: "Новая заявка" });
+    $("#rate").on('keyup', function(event) {
+        var allowedChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ","];
+        if (event.keyCode !== 8 && event.which !== 8) {
+            var value = event.currentTarget.value;
+            var newValue = "";
+            for (var i = 0; i < value.length; i++) {
+                if (allowedChars.indexOf(value[i]) !== -1) {
+                    newValue += value[i];
+                }
+            }
+            if (newValue.toString().length === 2) {
+                newValue += ".";
+            }
+            event.currentTarget.value = newValue;
+        }
+    });
 };
 Template.newOrderPage.events({
 	'submit': function(event) {
